ce4b6da1f97b118a7ddbb27a0d6f3ab9b2f149ca
Jodie Alaine Parker <me@jodiealaine.com>
2016-08-16 19:27:35 +0100

    Make filteredListings Observable and add functions to filter and reset listings and markers

--- js/main.js
+++ js/main.js
@@ -3,6 +3,95 @@ var map;
 // Create a new blank array for all the listing markers
 var markers = [];
 
+function AppViewModel() {
+  var self = this;
+  self.listings = [
+    {title: 'Mount Zion Coffee', location: {lat: 50.215332, lng: -5.477701}, type: 'coffee'},
+    {title: 'The Pier Coffee Bar', location: {lat: 50.215524, lng: -5.476800}, type: 'coffee'},
+    {title: 'The Coffee Lounge', location: {lat: 50.211323, lng: -5.479729}, type: 'coffee'},
+    {title: 'Yellow Canary', location: {lat: 50.213087, lng: -5.480501}, type: 'coffee'},
+    {title: 'The Hub', location: {lat: 50.213322, lng: -5.479873}, type: 'coffee'},
+    {title: 'Porthmeor Beach Cafe', location: {lat: 50.215052, lng: -5.482913}, type: 'coffee'},
+    {title: 'Tate Gallery', location: {lat: 50.214851, lng: -5.481918}, type: 'art'},
+    {title: 'Porthminster Gallery', location: {lat: 50.211720, lng:  -5.478527}, type: 'art'},
+    {title: 'Imagianation Gallery', location: {lat: 50.214487, lng: -5.478946}, type: 'art'},
+    {title: 'Newstatesman Gallery', location: {lat: 50.213759, lng: -5.480480}, type: 'art'},
+    {title: 'Whistlefish Gallery', location: {lat: 50.214652, lng: -5.479557}, type: 'art'},
+    {title: 'Belgrave Gallery', location: {lat: 50.213447, lng: -5.480394}, type: 'art'},
+    {title: 'Porthmeor Beach', location: {lat: 50.215620, lng: -5.482216}, type: 'beach'},
+    {title: 'St Ives Harbour Beach', location: {lat: 50.214999, lng: -5.478533}, type: 'beach'},
+    {title: 'Bamaluz Beach', location: {lat: 50.215866, lng: -5.476022}, type: 'beach'},
+    {title: 'Porthgwidden Beach', location: {lat: 50.217098, lng: -5.476279}, type: 'beach'},
+    {title: 'Porthminster Beach', location: {lat: 50.208623, lng: -5.475749}, type: 'beach'}
+  ];
+
+  self.filteredListings = ko.observableArray(self.listings);
+
+  // Filter the ListView
+  self.filterListView = function(type) {
+    self.filteredListings([]);
+    ko.utils.arrayForEach(self.listings, function(listing) {
+      if (listing.type == type) {
+        self.filteredListings.push(listing);
+      } 
+    });
+  }
+
+  self.resetListings = function() {
+    self.filteredListings([]);
+    ko.utils.arrayForEach(self.listings, function(listing) {
+        self.filteredListings.push(listing);
+    });
+
+    self.resetMarkers();
+  }
+
+  self.resetMarkers = function() {
+    ko.utils.arrayForEach(self.markers, function(marker) {
+      if (marker.getVisible() === true) {
+        change = true;
+      }
+      marker.setVisible(true);
+    });
+  }
+
+  self.filterMarkers = function(type) {
+    // Filter the markers
+    ko.utils.arrayForEach(self.markers, function(marker) {
+        if (type === marker.type) {
+            if (marker.getVisible() === true) {
+                change = true;
+            }
+            marker.setVisible(true);
+        } else {
+            if (marker.getVisible() === false) {
+                change = true;
+            }
+            marker.setVisible(false);
+        }
+    });
+  }
+  
+  // This function will update the filter applied to the listView and Markers
+  self.filterListings = function(type) {
+    switch(type){
+      case 'beach':
+        self.filterListView(type);
+        break;
+      case 'art':
+        self.filterListView(type);
+        break; 
+      case 'coffee':
+        self.filterListView(type);
+        break;
+    }    
+
+    self.filterMarkers(type);
+  };
+}
+
+ko.applyBindings(AppViewModel);
+
 function initMap() {
   // Create custom styles for the map
   var styles = [
@@ -77,62 +166,49 @@ function initMap() {
     }
   ];
 
-  // Custom styling for markers
-  var defaultIcon = makeMarkerIcon('7dcdcd');
-  var highlightedIcon = makeMarkerIcon('FFFF24');
-
-  // Initial hardcoded locations for area v1 coffee shops
-  // To be replaced by api data
-  var locations = [
-    {title: 'Mount Zion Coffee', location: {lat: 50.215332, lng: -5.477701}, type: 'coffee'},
-    {title: 'The Pier Coffee Bar', location: {lat: 50.215524, lng: -5.476800}, type: 'coffee'},
-    {title: 'The Coffee Lounge', location: {lat: 50.211323, lng: -5.479729}, type: 'coffee'},
-    {title: 'Yellow Canary', location: {lat: 50.213087, lng: -5.480501}, type: 'coffee'},
-    {title: 'The Hub', location: {lat: 50.213322, lng: -5.479873}, type: 'coffee'},
-    {title: 'Porthmeor Beach Cafe', location: {lat: 50.215052, lng: -5.482913}, type: 'coffee'},
-    {title: 'Tate Gallery', location: {lat: 50.214851, lng: -5.481918}, type: 'art'},
-    {title: 'Porthminster Gallery', location: {lat: 50.211720, lng:  -5.478527}, type: 'art'},
-    {title: 'Imagianation Gallery', location: {lat: 50.214487, lng: -5.478946}, type: 'art'},
-    {title: 'Newstatesman Gallery', location: {lat: 50.213759, lng: -5.480480}, type: 'art'},
-    {title: 'Whistlefish Gallery', location: {lat: 50.214652, lng: -5.479557}, type: 'art'},
-    {title: 'Belgrave Gallery', location: {lat: 50.213447, lng: -5.480394}, type: 'art'},
-    {title: 'Porthmeor Beach', location: {lat: 50.215620, lng: -5.482216}, type: 'beach'},
-    {title: 'St Ives Harbour Beach', location: {lat: 50.214999, lng: -5.478533}, type: 'beach'},
-    {title: 'Bamaluz Beach', location: {lat: 50.215866, lng: -5.476022}, type: 'beach'},
-    {title: 'Porthgwidden Beach', location: {lat: 50.217098, lng: -5.476279}, type: 'beach'},
-    {title: 'Porthminster Beach', location: {lat: 50.208623, lng: -5.475749}, type: 'beach'}
-  ];
+  
 
   var largeInfowindow = new google.maps.InfoWindow();
 
   // Add the location markers on initialize
-  for (var i = 0; i < locations.length; i++) {
-    // Get the position from the location array.
-    var position = locations[i].location;
-    var title = locations[i].title;
-    // Create a marker per location, and put into markers array.
-    var marker = new google.maps.Marker({
-      position: position,
-      title: title,
-      animation: google.maps.Animation.DROP,
-      icon: defaultIcon,
-      id: i
-    });
-    // Push the marker to our array of markers.
-    markers.push(marker);
-    // Create an onclick event to open the large infowindow at each marker.
-    marker.addListener('click', function() {
-      populateInfoWindow(this, largeInfowindow);
-    });
-    // Two event listeners - one for mouseover, one for mouseout,
-    // to change the colors back and forth.
-    marker.addListener('mouseover', function() {
-      this.setIcon(highlightedIcon);
-    });
-    marker.addListener('mouseout', function() {
-      this.setIcon(defaultIcon);
-    });
-  }
+    // Custom styling for markers
+    var defaultIcon = makeMarkerIcon('7dcdcd');
+    var highlightedIcon = makeMarkerIcon('FFFF24');
+
+    for (var i = 0; i < listings.length; i++) {
+      // Get the position from the location array.
+      var position = listings[i].location;
+      var title = listings[i].title;
+      var type = listings[i].type;
+
+      // Create a marker per location, and put into markers array.
+      var marker = new google.maps.Marker({
+        position: position,
+        title: title,
+        type: type,
+        visible: true,
+        animation: google.maps.Animation.DROP,
+        icon: defaultIcon,
+        id: i
+      });
+
+      // Add marker to our array of markers.
+      markers.push(marker);
+
+      // Create an onclick event to open the large infowindow at each marker.
+      marker.addListener('click', function() {
+        populateInfoWindow(this, largeInfowindow);
+      });
+      // Two event listeners - one for mouseover, one for mouseout,
+      // to change the colors back and forth.
+      marker.addListener('mouseover', function() {
+        this.setIcon(highlightedIcon);
+      });
+      marker.addListener('mouseout', function() {
+        this.setIcon(defaultIcon);
+      });
+    }
+
 
   // This function will loop through the markers array and display them all.
   function showListings() {
